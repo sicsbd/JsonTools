ac124bf0b4ae373d6b02085eaf28ad92225103f1
Rafsanul Hasan <rafsanulhasan@outlook.com>
2018-07-28 02:28:38 +0600

    Added Search support

---
+++ Symbiosis.Json.Tools/AspNetCore/Mvc/SearchTerm.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Abstractions;
+
+namespace Microsoft.AspNetCore.Mvc
+{
+	public class SearchTerm
+		 : QueryOptionsTerm
+	{
+		public string Operator { get; set; }
+
+
+		public string Value { get; set; }
+
+		public bool ValidSyntax { get; set; }
+
+		public ISearchExpressionProvider ExpressionProvider { get; set; }
+	}
+}
