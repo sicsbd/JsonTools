c78295c4cbfe8a899a279637890de89ab5bb6286
Rafsanul Hasan <rafsanulhasan@outlook.com>
2018-07-28 02:17:13 +0600

    Added abstraction layer

---
+++ Symbiosis.Json.Tools.Abstractions/IQueryStringOptionsProcessor{T,TEntity}.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.AspNetCore.Mvc.Abstractions
+{
+	public interface IQueryStringOptionsProcessor<T, TEntity, TTerm>
+		  where TTerm : QueryOptionsTerm, new()
+	{
+		void SetQuery(string[] query);
+
+		IEnumerable<TTerm> GetAllTerms();
+		IEnumerable<TTerm> GetValidTerms();
+		IQueryable<TEntity> Apply(IQueryable<TEntity> query);
+	}
+}
---
+++ Symbiosis.Json.Tools.Abstractions/QueryStringOptions{T,TEntity}.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Microsoft.AspNetCore.Mvc.Abstractions
+{
+	public abstract class QueryStringOptions<T, TEntity, TOptionsProcessor, TTerm>
+		 : IValidatableObject
+		where TOptionsProcessor : class, IQueryStringOptionsProcessor<T, TEntity, TTerm>, new()
+		where TTerm : QueryOptionsTerm, new()
+	{
+		protected TOptionsProcessor _processor { get; set; }
+
+		public string[] _queryStrings { get; protected set; }
+		public abstract IEnumerable<ValidationResult> Validate(ValidationContext validationContext);
+
+		public virtual IQueryable<TEntity> Apply(IQueryable<TEntity> query) 
+			=> _processor.Apply(query);
+	}
+}
